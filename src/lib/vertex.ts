549e5fba04bf8004f07667aa0be9729396cd771b
Kushal Mondal <kushalmondal349@gmail.com>
2025-09-21 02:31:18 +0530

    build error fix

--- src/lib/vertex.ts
+++ src/lib/vertex.ts
@@ -1,18 +1,18 @@
 import { VertexAI } from "@google-cloud/vertexai";
 
-const vertex = new VertexAI({
-  project: process.env.GOOGLE_PROJECT_ID!,
-  location: "asia-south1",
-  // googleAuthOptions: {
-  //   keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS, // file path
-  // },
-});
-
-export const careerModel = vertex.getGenerativeModel({
-  model: "gemini-2.5-flash",
-});
 
 export async function askVertex(prompt: string): Promise<string> {
+  const vertex = new VertexAI({
+    project: process.env.GOOGLE_PROJECT_ID!,
+    location: "asia-south1",
+    // googleAuthOptions: {
+    //   keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS, // file path
+    // },
+  });
+  
+   const careerModel = vertex.getGenerativeModel({
+    model: "gemini-2.5-flash",
+  });
   const resp = await careerModel.generateContent({
     contents: [{ role: "user", parts: [{ text: prompt }] }],
   });
